087781a44c6e232b55dfb089b50dc19ae0701276
Ashu Prakash <aspksh@gmail.com>
2026-08-06 23:13:04 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,53 @@
+class TrafficSignCNN(nn.module):
+    def __init__(self):
+        super(TrafficSignCNN, self).__init__()
+
+        self.block1 = Sequential(
+            nn.Conv2d(3, 32, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(32, 32, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Dropout(0.5)
+            )
+
+        self.block2 = Sequential(
+            nn.Conv2d(3, 64, kernel_size = 3, padding = 1),
+            nn.BatchNormd2d(64),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(64, 64, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace = True),
+            nn.MaxPool2d(kernel_size = 2, stride = 2),
+            nn.Dropout(0.5)
+            )
+
+        self.block3 = Sequential(
+            nn.Conv2d(3, 128, kernel_size = 3, padding = 1),
+            nn.BatchNormd2d(128),
+            nn.ReLU(inplace = True),
+            nn.Conv2d(128, 128, kernel_size = 3, padding = 1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace = True),
+            nn.MaxPool(kernel_size = 2, stride = 2),
+            nn.Dropout(0.5)
+            )
+
+       self.fc = Sequential(
+            nn.Flatten(),
+            nn.Linear(4 * 4 * 128, 512),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace = True),
+            nn.Dropout(0.5),
+            nn.Linear(512, 43)
+            )
+        
+    def forward(self, x):
+        x = self.block1(x)
+        x = self.block2(x)
+        x = self.block3(x)
+        x = self.fc(x)
+        return x
+        
