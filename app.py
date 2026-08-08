4af848908fbdd24cf285b9372b05be50c7667893
Ashu Prakash <aspksh@gmail.com>
2026-08-08 10:52:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ elif camera_image is not None:
   image = Image.open(camera_image)
 
 if image is not None:
-  img_tensor = transform(image).unsqueeze(0).to(device)  
+  img_tensor = transform(image).unsqueeze(0)  
 
   with torch.no_grad():
     
